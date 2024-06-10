a5ff9687eadaf1f5f9f6ef29f5d20a0f80f2ddcf
89fernando <fernando89soares@gmail.com>
2024-06-10 17:23:03 -0300

    Registering a ticket at the bank with server action

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -1,8 +1,49 @@
 import { Container } from "@/components/container";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import prismaClient from "@/lib/prisma";
 
+export default async function NewTicket() {
+    const session = await getServerSession(authOptions);
 
-export default function NewTicket() {
+    if(!session || !session.user) {
+
+        redirect("/")
+    }
+
+    const customers = await prismaClient.customer.findMany({
+        where: {
+            userId: session.user.id
+        }
+    })
+
+    async function handleRegisterTicket(formData: FormData) {
+        "use server"
+        const name = formData.get("name")
+        const description = formData.get("description")
+        const customerId = formData.get("customer")
+
+        if(!name || !description || !customerId) {
+            return;
+        }
+
+        await prismaClient.ticket.create({
+            data: {
+                name: name as string,
+                description: description as string,
+                customerId: customerId as string,
+                status:"ABERTO",
+                userId: session?.user.id
+            }
+        })
+        redirect('/dashboard')
+        
+    }
+
+    
+    
     return (
         <Container>
             <main className="mt-9 mb-2">
@@ -13,13 +54,14 @@ export default function NewTicket() {
                     <h1 className="text-3xl font-bold">Novo chamado</h1>
                 </div>
 
-                <form className="flex flex-col mt-6">
+                <form className="flex flex-col mt-6" action={handleRegisterTicket}>
                     <label className="mb-1 font-medium text-lg">Novo chamado</label>
                     <input 
                         className="w-full border-2 rounded-md px-2 mb-2 h-11"
                         type="text"
                         placeholder="Digite o nome do chamado"
                         required
+                        name='name'
                     />
                     
                     <label className="mb-1 font-medium text-lg">Descreva o problema</label>
@@ -27,12 +69,41 @@ export default function NewTicket() {
                         className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none"
                         placeholder="Descreva o problema"
                         required
+                        name="description"
                     />
 
-                    <label className="mb-1 font-medium text-lg">Selecione o cliente</label>
-                    <select className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white">
-                        <option value="teste1">Teste</option>
-                    </select>
+                    {customers.length !== 0 && (
+                        <>
+                            <label className="mb-1 font-medium text-lg">Selecione o cliente</label>
+                            <select 
+                                className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white" 
+                                name='customer'
+                            >
+                                {customers.map( customer => (
+                                    <option
+                                    key={customer.id}
+                                    value={customer.id}
+                                >
+                                    {customer.name}
+                                </option>
+                                ))
+
+                                }
+                            </select>
+                        </>
+                    )}
+                    {customers.length === 0 && (
+                        <Link href='/dashboard/customer/new'>
+                            Você ainda não tem nenhum cliente. <span className="text-blue-500 font-medium">Cadastrar cliente.</span>
+                        </Link>
+                    )}
+
+                    <button
+                        type="submit"
+                        className="bg-blue-500 text-white px-2 my-4 disabled:cursor-not-allowed h-11 rounded"
+                    >
+                        Cadastrar
+                    </button>
                 </form>
             </main>
         </Container>
